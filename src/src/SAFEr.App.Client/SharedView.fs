4f1ae5cdb698951c0fe391ae83613167f7f201ad
Roman Provazník <dzoukr@dzoukr.cz>
2020-12-13 11:13:17 +0100

    Dependencies update

--- src/src/SAFEr.App.Client/SharedView.fs
+++ src/src/SAFEr.App.Client/SharedView.fs
@@ -6,14 +6,13 @@ open Router
 type prop
     with
         static member inline href (p:Page) = prop.href (p |> Page.toUrlSegments |> Router.formatPath)
-        static member inline onClick (p:Page) = prop.onClick (Router.goToUrl)
 
 type Html
     with
         static member inline a (text:string, p:Page) =
             Html.a [
                 prop.href p
-                prop.onClick p
+                prop.onClick Router.goToUrl
                 prop.text text
             ]
 
