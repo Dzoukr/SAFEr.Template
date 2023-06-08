f063c428456ec9598cf4988359a529de0449fe2b
Jiří Landsman <landsman@cngroup.dk>
2023-06-08 11:48:42 +0200

    refactor the default template Startup
    use the new application startup model using the WebApplicationBuilder

--- src/default/src/SAFEr.App.Server/Program.fs
+++ src/default/src/SAFEr.App.Server/Program.fs
@@ -1,21 +1,40 @@
 module SAFEr.App.Server.Program
 
+open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
 
-[<EntryPoint>]
-let main _ =
-    Host.CreateDefaultBuilder()
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .UseStartup(typeof<Startup.Startup>)
-                    .ConfigureLogging(fun x ->
-                        x.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider> ("", LogLevel.Information) |> ignore
-                    )
-                    .UseWebRoot("public")
-                    |> ignore)
-        .Build()
-        .Run()
-    0
+let private configureLogging (builder: WebApplicationBuilder) =
+    builder.Host.ConfigureLogging(fun x ->
+        x.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider> ("", LogLevel.Information) |> ignore
+    ) |> ignore
+    builder
+
+let private addApplicationInsights (builder:WebApplicationBuilder) =
+    builder.Services.AddApplicationInsightsTelemetry() |> ignore
+    builder
+
+let private configureWeb (builder:WebApplicationBuilder) =
+    builder.Services.AddGiraffe() |> ignore
+    builder
+
+let private configureApp (app:WebApplication) =
+    app.UseStaticFiles() |> ignore
+    app.UseGiraffe WebApp.webApp
+    app
+
+let builderOptions = WebApplicationOptions(WebRootPath = "public")
+let builder =
+    WebApplication.CreateBuilder(builderOptions)
+    |> addApplicationInsights
+    |> configureLogging
+    |> configureWeb
+
+let app =
+    builder.Build()
+    |> configureApp
+
+app.Run()
