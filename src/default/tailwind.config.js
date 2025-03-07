6615fa7b3189e78a0c07ab4858466376bed8036e
Roman Provazník <dzoukr@dzoukr.cz>
2025-03-07 12:24:23 +0100

    v5.0.0 - Latest .NET, Daisy, Vite, React, and Tailwind

--- src/default/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import daisyui from 'daisyui'
-export default {
-    content: [
-        "./src/SAFEr.App.Client/.fable-build/**/*.{js,ts,jsx,tsx}",
-    ],
-    plugins: [
-        daisyui,
-    ]
-}
--- src/default/vite.config.js
+++ src/default/vite.config.js
@@ -1,14 +1,20 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-    plugins: [react({ jsxRuntime: 'classic'})], // jsxRuntime: 'classic' is required for fast-refresh for .js files
+    plugins: [tailwindcss(),react({ jsxRuntime: 'classic'})], // jsxRuntime: 'classic' is required for fast-refresh for .js files
     root: "./src/SAFEr.App.Client",
     server: {
         port: 8080,
         proxy: {
             '/api': 'http://localhost:5000',
+        },
+        watch: {
+            ignored: [
+                "**/*.fs" // Don't watch F# files
+            ]
         }
     },
     build: {
